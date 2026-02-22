bf88f1347c073f2c79f9e8900572ec67cdd0904a
豊田 真吾 <toyodasinngo@gmail.com>
2026-02-22 14:53:31 +0900

    Initial Commit

---
+++ HakodateSwift/HakodateSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HakodateSwiftApp.swift
+//  HakodateSwift
+//
+//  Created by Shingo Toyoda on 2026/02/22.
+//
+
+import SwiftUI
+
+@main
+struct HakodateSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
